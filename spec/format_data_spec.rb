c2fb198937e42b9cb6cf70aca26b17c25fbc4fce
Kai Marshland <kaimarshland@gmail.com>
2018-12-31 22:38:22 -0700

    Wrote tool to format data properly from file

--- spec/format_data_spec.rb
+++ spec/format_data_spec.rb
@@ -3,6 +3,64 @@ require_relative '../src/errors.rb'
 
 describe 'format_data' do
 
+  describe 'when converting to ranked choices' do
+    it 'works on sample-1' do
+      data = File.readlines('spec/data/sample-ballots-1.tsv')
+      pairings = extract_co_prez_selections File.readlines('spec/data/sample-pairings-1.txt')
+      result = convert_to_ranked_choices data, pairings
+
+      expect(result).to eq([
+          ['C + D', 'A + B', 'A + B', 'D + E', 'A + D'],
+          ['A + D', 'C + D', 'A + B', 'A + B', 'D + E'],
+          ['A + D', 'C + D', 'A + B', 'D + E', 'A + B'],
+          ['D + E', 'C + E', 'B + C', 'A + D', 'A + E', 'A + C', 'B + D', 'A + B', 'B + E', 'C + D'],
+          ['A + B', 'B + E', 'A + E', 'A + C', 'C + E', 'D + E', 'C + D', 'B + D', 'B + C'],
+          ['A + E', 'B + E', 'B + C']
+                           ])
+    end
+
+    it '#extract_ranked_choices extracts the choices for a line' do
+      columns = {
+          0 => 'A',
+          1 => 'B',
+          2 => 'C'
+      }
+
+      expect(extract_ranked_choices(['First Choice', 'Second Choice', 'Third Choice'], columns)).to eq(['A', 'B', 'C'])
+      expect(extract_ranked_choices(['Third Choice', 'Second Choice', 'First Choice'], columns)).to eq(['C', 'B', 'A'])
+      expect(extract_ranked_choices(['Third Choice', 'Second Choice', ''], columns)).to eq(['B', 'A'])
+    end
+
+    it 'can convert a rank string to an integer' do
+      expect(convert_rank_to_integer('First Choice')).to eq(1)
+      expect(convert_rank_to_integer('Second Choice')).to eq(2)
+      expect(convert_rank_to_integer('Third Choice')).to eq(3)
+      expect(convert_rank_to_integer('Fourth Choice')).to eq(4)
+      expect(convert_rank_to_integer('Fifth Choice')).to eq(5)
+      expect(convert_rank_to_integer('Sixth Choice')).to eq(6)
+      expect(convert_rank_to_integer('Seventh Choice')).to eq(7)
+      expect(convert_rank_to_integer('Eighth Choice')).to eq(8)
+      expect(convert_rank_to_integer('Ninth Choice')).to eq(9)
+      expect(convert_rank_to_integer('Tenth Choice')).to eq(10)
+    end
+
+    it 'throws an error when converting an unknown rank string' do
+      expect{ convert_rank_to_integer('Blork')}.to raise_error(DataValidationError)
+    end
+  end
+
+  describe '#extract_co_prez_selections' do
+    it 'calls validate' do
+      expect{
+        validate_pairings! ['bogus']
+      }.to raise_error(DataValidationError)
+    end
+
+    it 'extracts selections' do
+      expect(extract_co_prez_selections(['A + B', 'B + A', '', '  ', '# comment'])).to eq({'A' => 'B', 'B' => 'A'})
+    end
+  end
+
   describe 'when monkey patching String' do
     it 'creates a beats method' do
       expect('A'.beats('B')).to eq(['A', 'B'])
@@ -56,12 +114,15 @@ describe 'format_data' do
                                'Timestamp	Would you like to rank a full list of all 10 possible co-president candidate pairs, or to rank a single list with the five candidates?	Your Vote [A + B]	Your Vote [B + A]	Your Vote [A + D]	Your Vote [A + E]	Your Vote [B + C]	Your Vote [B + D]	Your Vote [B + E]	Your Vote [C + D]	Your Vote [C + E]	Your Vote [D + E]	Your Vote [A]	Your Vote [B]	Your Vote [C]	Your Vote [D]	Your Vote [F]	Select Two Financial Officers	Ian Gomez?'
                            ]
         }.to raise_error(DataValidationError)
+      end
 
-        # expect{
-        #   validate_ballot! [
-        #                        'Timestamp	Would you like to rank a full list of all 10 possible co-president candidate pairs, or to rank a single list with the five candidates?	Your Vote [A + B]	Your Vote [A + C]	Your Vote [A + D]	Your Vote [A + E]	Your Vote [B + C]	Your Vote [B + D]	Your Vote [B + E]	Your Vote [C + D]	Your Vote [C + E]	Your Vote [D + E]	Your Vote [A]	Your Vote [B]	Your Vote [C]	Your Vote [D]	Your Vote [E]	Select Two Financial Officers	Ian Gomez?'
-        #                    ]
-        # }.to raise_error(DataValidationError)
+      it 'makes each line selects a valid ranking option' do
+        expect{
+          validate_ballot! [
+                               'Timestamp	Would you like to rank a full list of all 10 possible co-president candidate pairs, or to rank a single list with the five candidates?	Your Vote [A + B]	Your Vote [A + C]	Your Vote [A + D]	Your Vote [A + E]	Your Vote [B + C]	Your Vote [B + D]	Your Vote [B + E]	Your Vote [C + D]	Your Vote [C + E]	Your Vote [D + E]	Your Vote [A]	Your Vote [B]	Your Vote [C]	Your Vote [D]	Your Vote [E]	Select Two Financial Officers	Ian Gomez?',
+                               '12/31/2018 18:15:55	Something spooky											Second Choice	Third Choice	First Choice	Fifth Choice	Fourth Choice	'
+                           ]
+        }.to raise_error(DataValidationError)
       end
     end
 
--- src/format_data.rb
+++ src/format_data.rb
@@ -1,5 +1,87 @@
 require_relative 'errors'
 
+# the index of the column that asks if you are using
+OPTION_INDEX = 1
+
+# the regex that says they are ranking all candidate pairings
+RANK_ALL = /I would like to rank all \d+ pairs/
+
+# the regex that says they are ranking individual candidates and turning it over to the co-prezes
+RANK_CO_PREZ = /I would like to rank the \d+ individual candidates/
+
+def convert_to_ranked_choices(lines, co_prez_selections)
+  validate_ballot! lines
+
+  column_to_candidates = map_columns_to_candidates lines.first
+  individual_candidate_columns = column_to_candidates.select{|_index, candidate| /^\w+$/.match? candidate}.to_h
+  paired_candidate_columns = column_to_candidates.reject{|_index, candidate| /^\w+$/.match? candidate}.to_h
+
+  all_ranked_choices = []
+
+  lines.drop(1).each do |line|
+    parts = line.split("\t")
+
+    ranking_all = RANK_ALL.match? parts[OPTION_INDEX]
+
+    if ranking_all
+      ranked_choices = extract_ranked_choices parts, paired_candidate_columns
+    else
+      ranked_choices = extract_ranked_choices parts, individual_candidate_columns
+
+      # pair the candidates up
+      ranked_choices.map! {|candidate| "#{candidate} + #{co_prez_selections[candidate]}"}
+    end
+
+    # alphabetize candidate pairs
+    ranked_choices.map! {|pair| pair.split(' + ').sort.join(' + ')}
+
+    all_ranked_choices << ranked_choices
+  end
+
+  all_ranked_choices
+end
+
+# Given an array of parts of a line, and a map from column to candidate, figure out how to order the specified candidates as an array
+def extract_ranked_choices(parts, relevant_columns)
+  ranking = {}
+
+  relevant_columns.each do |index, candidate|
+    next if index >= parts.length # tsv will truncate unnecessary columns
+    rank = parts[index].strip
+
+    ranking[convert_rank_to_integer rank] = candidate unless rank.empty?
+  end
+
+  # convert to an array, sorted by ranking
+  ranking.sort_by{|rank, _candidate| rank}.to_h.values
+end
+
+# Given a rank as a string (eg "First Choice"), turn it into an integer (eg 1)
+def convert_rank_to_integer(rank_string)
+  lookup = %w(First Second Third Fourth Fifth Sixth Seventh Eighth Ninth Tenth)
+  rank = lookup.index(rank_string.split(' ').first) # find index of first word of the string
+
+  raise DataValidationError.new("Invalid rank string: #{rank_string}") if rank.nil?
+
+  rank + 1 # add 1 for consistency
+end
+
+# Takes lines from a file and converts them to a hash where the key is a candidate and the value is their ideal pairing
+def extract_co_prez_selections(lines)
+  validate_pairings! lines
+
+  selections = {}
+  lines.each do |line|
+    next if line.strip.empty?
+    next if line[0] == '#'
+
+    first, second = line.split(' + ')
+    selections[first.strip] = second.strip
+  end
+
+  selections
+end
+
 # monkey patch String so that it's easier to keep track of the ordering in beats vs loses to
 class String
   def beats(other)
@@ -63,6 +145,13 @@ def validate_ballot!(lines)
 
   # verify that there are no leftovers
   raise DataValidationError.new("Excess candidate pairs (#{candidate_pairs.join(', ')})") unless candidate_pairs.empty?
+
+  # check that each ballot selects an option
+  lines.drop(1).each do |line|
+    option = line.split("\t")[OPTION_INDEX]
+
+    raise DataValidationError.new("Invalid selection: #{option}") unless RANK_ALL.match?(line) || RANK_CO_PREZ.match?(line)
+  end
 end
 
 # Validates that a set of lines (strings) representing co-president tie-breaking config is in the right format
